6c1844f7f4b983f638b118fe13a0ea9e985dc61c
Carsten Sandker <carsten.sandker@googlemail.com>
2014-02-24 17:59:10 +0100

    PHP Grundlagen Beispiel hinzugefuegt + Struktur verbessert

---
+++ Betrieb von Informations- und Kommunikationssystemen/Listings/PHP_Grundlagen.php
@@ -0,0 +1,6 @@
+<?php
+  $ergebnis = //Benutzeranfrage...
+  if($ergebnis){
+    echo "<p>Die Benutzeranfrage wurde positiv ausgewertet</p>";
+  }else echo "<p>Die Benutzeranfrage wurde negativ ausgewertet</p>"
+?>
