0d4bd7e9fb9a352cacefe8d7fdd99a8e8b8e7f48
Carsten Sandker <carsten.sandker@googlemail.com>
2014-02-26 23:52:16 +0100

    Fehler verbessert

--- Betrieb von Informations- und Kommunikationssystemen/Listings/PHP_Grundlagen.php
+++ Betrieb von Informations- und Kommunikationssystemen/Listings/PHP_Grundlagen.php
@@ -2,5 +2,8 @@
   $ergebnis = //Benutzeranfrage...
   if($ergebnis){
     echo "<p>Die Benutzeranfrage wurde positiv ausgewertet</p>";
-  }else echo "<p>Die Benutzeranfrage wurde negativ ausgewertet</p>"
+  }
+  else{
+    echo "<p>Die Benutzeranfrage wurde negativ ausgewertet</p>";
+  }
 ?>
